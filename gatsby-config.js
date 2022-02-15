9b4446b30598539a1393de1e0835c6c9ab8f7f38
Dennis <56740920+xrandie@users.noreply.github.com>
2022-02-16 02:36:40 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Dennis Ndegwa',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'I am a software engineer who specializes in designing and building exceptional digital experiences.',
+    siteUrl: 'https://xrandie.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@6saint_',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'DennisNdegwa',
+        short_name: 'DennisNdegwa',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
